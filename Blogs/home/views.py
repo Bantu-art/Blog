cbe7556201337c2b810a1952b674ea3ed9fe6326
Bantu-art <brianbaraza40@gmail.com>
2025-04-16 14:03:03 +0300

    Update URL routing and home view template for blog posts

--- Blogs/home/views.py
+++ Blogs/home/views.py
@@ -3,4 +3,4 @@ from .models import BlogPost
 
 def home(request):
     posts = BlogPost.objects.all().order_by('-created_at')  # latest first
-    return render(request, 'blog/home.html', {'posts': posts})
+    return render(request, 'home/home.html', {'posts': posts})
