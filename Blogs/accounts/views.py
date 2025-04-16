4bdf342548d6cc79ed366adc965f9da957f6b18f
Bantu-art <brianbaraza40@gmail.com>
2025-04-16 14:20:04 +0300

    Add login view to accounts app with authentication functionality

--- Blogs/accounts/views.py
+++ Blogs/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login
 
 def register_view(request):
@@ -12,3 +12,14 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
