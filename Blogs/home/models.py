9a8479296aa05d488d41d772a14fbac982d4f823
Bantu-art <brianbaraza40@gmail.com>
2025-04-17 22:46:35 +0300

    Add Comment model to BlogPost for user comments and replies

--- Blogs/home/models.py
+++ Blogs/home/models.py
@@ -7,10 +7,20 @@ class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)  # Track likes
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return self.title
 
     def total_likes(self):
-        return self.likes.count()  # Helper method to get total likes
+        return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
