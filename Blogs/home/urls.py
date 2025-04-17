3aeab9f777921e6e34f38ef58c20cdce5edc7dce
Bantu-art <brianbaraza40@gmail.com>
2025-04-17 22:48:37 +0300

    Add URL pattern for adding comments to blog posts

--- Blogs/home/urls.py
+++ Blogs/home/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.create_blog_post, name='create_blog_post'),
-    path('like/<int:post_id>/', views.like_post, name='like_post'), 
+    path('like/<int:post_id>/', views.like_post, name='like_post'),
+    path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
 ]
