5106440d0a7e0ac25525f99b0c8d94f55f398a95
Bantu-art <brianbaraza40@gmail.com>
2025-04-17 22:13:41 +0300

    Add likes functionality to BlogPost model with total likes method

--- Blogs/home/models.py
+++ Blogs/home/models.py
@@ -7,6 +7,10 @@ class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)  # Track likes
 
     def __str__(self):
         return self.title
+
+    def total_likes(self):
+        return self.likes.count()  # Helper method to get total likes
